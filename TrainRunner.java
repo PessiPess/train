a72a9bfb55c927788e52fedf0667ede89b64f931
Panch <pessipess@gmail.com>
2017-09-17 13:08:52 +0300

    commit

--- TrainRunner.java
+++ TrainRunner.java
@@ -4,23 +4,19 @@ public class TrainRunner {
 
     public static int findLength(Train train){
         int lenght = 1;
-        //int currentPosition = 1;
         train.lightOn();
 
         while (train.isLightOn()){
 
             for (int i = 0; i < lenght+1; i++) {
                 train.turnLeft();
-                //currentPosition++;
             }
             train.lightOff();
 
             for (int i = 0; i < lenght + 1; i++) {
                 train.turnRight();
-                //currentPosition--;
             }
 
-            //lenght+= (train.isLightOn())?1:0;
             lenght++;
         }
 
@@ -30,7 +26,6 @@ public class TrainRunner {
 
     public static void main(String[] args) {
         ListOfWagons listOfWagons = new ListOfWagons(50);
-        //listOfWagons.output();
         Train train = listOfWagons;
 
         System.out.println(train.isLength(findLength(train)));
